1cbc99cc00dde31eda13031dd5f09d08546e1f69
Rockoban420 <lautaro.avellaneda@gmail.com>
2023-05-16 12:52:27 -0700

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,37 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+
+// const sequelize = new Sequelize(
+//   process.env.DB,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//   }
+// );
+
+
+// module.exports = sequelize;
+
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
+let sequelize;
 
-const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  }
-);
-
+// when deployed on Heroku
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // localhost
+    sequelize = new Sequelize(process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
